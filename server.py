69c94e7283e6ab469b4aad3ec89653cf2cb8c1fc
scoozer-ctrl <wyattc121@gmail.com>
2025-12-10 15:45:25 -0500

    Update server.py

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ from email.utils import formatdate
 
 PORT = 8000
 LOG_FILE = "access.log"
-DISCORD_WEBHOOK = ("https://discord.com/api/webhooks/1398083193265062020/OL-nBYHkOjSi8Nf50TJ9vA4UTyT6dhNcSTlF6vc4B-UEgmf8nHtnvMzj7DGd8iLiZDjt")  # Add your Discord webhook URL here
+DISCORD_WEBHOOK = ("https://discord.com/api/webhooks/1448408044651282530/IwbthMTuEVgNLstDzNUGs1grlux2cm1euZuc1I0avZE7m_qb3Vf6utcNony_BvS9uzpr")  # Add your Discord webhook URL here
 PIXEL_PATH = "/tracking_pixel.gif"
 PIXEL_DATA = b'GIF89a\x01\x00\x01\x00\x80\x00\x00\xff\xff\xff\x00\x00\x00!\xf9\x04\x01\x00\x00\x00\x00,\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02\x02D\x01\x00;'
 
